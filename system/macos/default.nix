227577dfc319d1fef402cf96ac5469734b44268b
Will Hopkins <willothyh@gmail.com>
2024-05-31 02:20:47 -0700

    enable touch id for sudo

--- system/macos/default.nix
+++ system/macos/default.nix
@@ -1,11 +1,13 @@
 { pkgs, globalUserInfo, ... }: {
-  imports = [ 
+  imports = [
     ./programs/_1password.nix
   ];
 
+  security.pam.enableSudoTouchIdAuth = true;
+
   programs.zsh.enable = true;
   programs.zsh.interactiveShellInit = ''
-      [[ -f "${globalUserInfo.homeDir}/Library/Application Support/amazon-q/shell/zshrc.post.zsh" ]] && builtin source "${globalUserInfo.homeDir}/Library/Application Support/amazon-q/shell/zshrc.post.zsh"
-    '';
+    [[ -f "${globalUserInfo.homeDir}/Library/Application Support/amazon-q/shell/zshrc.post.zsh" ]] && builtin source "${globalUserInfo.homeDir}/Library/Application Support/amazon-q/shell/zshrc.post.zsh"
+  '';
 }
 
