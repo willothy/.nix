ae12dddf323923f6520239f558038a9d00d7c4c2
Will Hopkins <willothyh@gmail.com>
2024-06-08 00:36:11 -0700

    configure ssh into orbstack

--- user/shared/programs/ssh.nix
+++ user/shared/programs/ssh.nix
@@ -1,18 +1,34 @@
 { config, pkgs, globalUserInfo, ... }: {
-  programs.ssh = let
-    socket = if (pkgs.system == "aarch64-darwin") then
-      "${globalUserInfo.homeDir}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-    else
-      "${globalUserInfo.homeDir}/.1password/agent.sock"
-    ;
-  in {
-    enable = true;
-    forwardAgent = true;
-    matchBlocks = {
-      "*" = {
-        host = "*"; 
-        extraOptions.IdentityAgent = ''"${socket}"'';
+  programs.ssh =
+    let
+      darwin = pkgs.system == "aarch64-darwin";
+      socket =
+        if darwin then
+          "${globalUserInfo.homeDir}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+        else
+          "${globalUserInfo.homeDir}/.1password/agent.sock"
+      ;
+      orbstack =
+        if darwin then
+          ''
+            Include ${globalUserInfo.homeDir}/.orbstack/ssh/config
+          ''
+        else
+          ''
+          ''
+      ;
+    in
+    {
+      enable = true;
+      forwardAgent = true;
+      matchBlocks = {
+        "*" = {
+          host = "*";
+          extraOptions.IdentityAgent = ''"${socket}"'';
+        };
       };
+      extraConfig = ''
+        ${orbstack}
+      '';
     };
-  };
 }
